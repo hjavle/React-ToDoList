d9598f0ef3a59764125e5aaa44f1abc5e57d5a54
Harsha Javle <hjavle@hotmail.com>
2020-12-09 13:11:28 -0500

    UI is accepted and form is reset

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 function App() {
   const [ inputText, setInputText] = useState("");
+  const [ todos, setTodos ] = useState([]);
   return (
     <div className="App">
        <header>
   <h1>Harsha's To Do List </h1>
     </header>
-      <Form setInputText = {setInputText}/>
+      <Form 
+        inputText = {inputText}
+        todos ={todos} 
+        setTodos={setTodos} 
+        setInputText = {setInputText}
+        />
       <TodoList />
     </div>
   );
